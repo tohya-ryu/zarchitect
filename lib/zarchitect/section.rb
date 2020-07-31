a450d74ea4076eaf9c05cb5c661b851352221762
ryu <ryu@tohya.net>
2020-07-31 11:55:54 +0200

    new method to ease section config access

--- lib/zarchitect/section.rb
+++ lib/zarchitect/section.rb
@@ -6,15 +6,20 @@ class Section < Zarchitect
   # @name | Name of section
   def initialize(name)
     @name = name
+    p self.config
   end
 
   def collection?
-    if @@config[:sections][:"#{@name}"].has_key?(:collection)
-      key = @@config[:sections][:"#{@name}"][:collection]
+    if self.config.has_key?(:collection)
+      key = self.config[:collection]
     else 
       key = false
     end
     key
+  end
+
+  def config
+    @@config[:sections][:"#{@name}"]
   end
 
 end
