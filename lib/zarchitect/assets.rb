09f15d7d89bb636f848c3e5f88857b2c5ef1cba6
= <=>
2020-08-10 12:56:04 +0200

    don't copy assets of filetyped specified in config

--- lib/zarchitect/assets.rb
+++ lib/zarchitect/assets.rb
@@ -5,8 +5,15 @@ module Assets
   def self.update
     files = Dir.files("_assets/")
     files.each do |f|
+      check = false
+      if Config.singleton_methods.include?(:exclude_assets)
+        Config.exclude_assets.each do |s|
+          check = true if f.ends_with?(s)
+          GPI.print "Ignoring asset #{f} because of its filetype"
+        end
+      end
+      next if check
       path = File.join("_assets", f)
-      #if File.fnmatch("*.css", path)
       path2 = File.join("_html/assets", f)
       if File.exist?(path2)
         if File.stat(path).mtime > File.stat(path2).mtime
@@ -15,7 +22,6 @@ module Assets
       else
         File.copy(path, path2)
       end
-      #end
     end
     GPI.print "Updated live assets"
   end
