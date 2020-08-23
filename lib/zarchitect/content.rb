ac448dd6e44a19557af4e781e084b1bbca55714b
ryu <ryu@tohya.net>
2020-08-23 18:04:06 +0200

    prepare implementation of content processing

--- lib/zarchitect/content.rb
+++ lib/zarchitect/content.rb
@@ -11,6 +11,67 @@ class Content < Zarchitect
 
   def markup
     @html = @raw
+    if string.is_a?(String)
+      new_string = ""
+      regexp = /
+        \A
+        MEDIA:(?<filetype>img|img_full|video|yt|audio):
+        (?<id>[a-zA-Z0-9|]+):"(?<caption>.*)":?(?<width>[0-9px%]*)
+        /x
+      string.each_line do |str|
+        m = regexp.match(str)
+        if m
+          # file tag found
+          # replace with corresponding html :)
+          # m[0] whole tag
+          @caption = m[:caption]
+          new_html = ""
+          case m[:filetype]
+          when 'img'
+            if m[:id].count('|') == @caption.count('|')
+              @images = ImageFile.find(m[:id].split('|'))
+              unless m[:width].empty?
+                @max_width = m[:width]
+              else
+                @max_width = '100%'
+              end
+              html = render_to_string "main/_image", layout: false
+            else
+              html = "failed to render media"
+            end
+          when 'img_full'
+            @image = ImageFile.find(m[:id])
+            html = render_to_string "main/_image_full", layout: false
+          when 'video'
+            @video = VideoFile.find(m[:id])
+            html = render_to_string "main/_video", layout: false
+          when 'audio'
+            @audio = AudioFile.find(m[:id])
+            html = render_to_string "main/_audio", layout: false
+          when 'yt'
+            @yt_id = m[:id]
+            html = render_to_string "main/_youtube", layout: false
+          else
+            html = "failed to render media"
+          end
+          html.each_line do |substr|
+            if substr.lstrip
+              new_html << substr.lstrip
+            else
+              new_html << substr
+            end
+          end
+          str.sub! m[0], new_html.chomp!
+        end
+        new_string << str
+      end
+
+      markdown = Redcarpet::Markdown.new(RougeHTML,
+                                         autolink: true)
+      return markdown.render(new_string)
+    else
+      return nil
+    end
   end
 
   private
