1bbc318e5eb7bf3999b8d29e51052b85b67f6d27
ryu <ryu@tohya.net>
2020-08-24 14:25:02 +0200

    create symlinks for files

--- lib/zarchitect/file_manager.rb
+++ lib/zarchitect/file_manager.rb
@@ -1,4 +1,4 @@
-class FileManager
+module FileManager
 
   FROM = "_files" # dir where raw files reside
   TO   = "_html/files"
@@ -13,6 +13,11 @@ class FileManager
       Util.mkdir(realpath) if File.directory?(fullpath)
       next if File.directory?(fullpath)
       # file handling
+      # create symlink in _html/files to physical files _files
+      unless File.symlink?(realpath)
+        symlink(File.join(Dir.getwd, fullpath), realpath)
+      end
+      # handle file types embedded in posts
       if Image.is_valid?(fullpath)
       elsif Audio.is_valid?(fullpath)
       elsif Video.is_valid?(fullpath)
@@ -21,10 +26,10 @@ class FileManager
 
     end
     # create dir copies in TO
-    # create symlinks in TO pointing to FROM | rsync can copy files behind link
   end
 
-  def iterate
+  def self.symlink(from, to)
+    File.symlink(from, to)
   end
 
 end
