20cba4e299467d5d4eea00795d76cc42f1cb8792
ryu <ryu@tohya.net>
2020-08-24 13:08:06 +0200

    set filemanager flow

--- lib/zarchitect/file_manager.rb
+++ lib/zarchitect/file_manager.rb
@@ -5,10 +5,20 @@ class FileManager
 
   def self.run
     # iterate FROM 
-    Dir[ File.join(FROM, '**', '*') ].reject do |f|
-      if File.directory?(f)
+    Dir[ File.join(FROM, '**', '*') ].reject do |fullpath|
+      path = fullpath[(FROM.length)..-1]
+      realpath = File.join(TO, path) # path of new dir/symlink
+      
+      # dir handling
+      Util.mkdir(realpath) if File.directory?(fullpath)
+      next if File.directory?(fullpath)
+      # file handling
+      if Image.is_valid?(fullpath)
+      elsif Audio.is_valid?(fullpath)
+      elsif Video.is_valid?(fullpath)
       else
       end
+
     end
     # create dir copies in TO
     # create symlinks in TO pointing to FROM | rsync can copy files behind link
