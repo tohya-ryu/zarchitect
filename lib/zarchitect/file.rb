dcdde16e94bc7b4840f84b9bb313f4fd9cf794f0
ryu <ryu@tohya.net>
2020-08-24 09:55:49 +0200

    setup img

--- lib/zarchitect/file.rb
+++ lib/zarchitect/file.rb
@@ -1,2 +1,2 @@
-class File
+class Zfile
 end
--- lib/zarchitect/image.rb
+++ lib/zarchitect/image.rb
@@ -1,2 +1,34 @@
+class ImageSet
+
+  def initialize(path)
+  end
+
+end
+
 class Image < File
+
+  #+++++++++++++++++++++++++++++
+  # @path
+  # @thumbl_path
+  # @thumbs_path
+  # @url
+  # @thumbl_url
+  # @thumbs_url
+  # @dimensions
+  # @thumbl_dimensions
+  # @thumbs_dimensions
+
+  def initialize(path)
+  end
+  
+end
+
+class Point
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
 end
