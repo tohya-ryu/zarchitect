421a367044afc86b5c79188ff7dbb7ae6e4a4833
ryu <ryu@tohya.net>
2020-08-26 10:40:22 +0200

    adjust markup method and media layout

--- lib/zarchitect/content.rb
+++ lib/zarchitect/content.rb
@@ -13,74 +13,96 @@ class Content < Zarchitect
     @html = @raw
     @img_id = 0
     @img_id_inc = 1
-    return nil
-    if string.is_a?(String)
-      new_string = ""
-      regexp = /
-        \A
-        MEDIA:(?<filetype>img|img_full|video|yt|audio):
-        (?<id>[a-zA-Z0-9|./_]+):"(?<caption>.*)":?(?<width>[0-9px%]*)
-        /x
-      string.each_line do |str|
-        m = regexp.match(str)
-        if m
-          # file tag found
-          # replace with corresponding html :)
-          # m[0] whole tag
-          @caption = m[:caption]
-          new_html = ""
-          case m[:filetype]
-          when 'img'
-            @img_id += @img_id_inc
-            @imgset = Array.new
-            if m[:id].count('|') == @caption.count('|')
-              m[:id].split('|').each do |url|
-                @imgset.push Image.find("url", id).split('|'))
-              end
-              @img_id_inc = @imgset.size
-              #@images = ImageFile.find(m[:id].split('|'))
-              unless m[:width].empty?
-                @max_width = m[:width]
-              else
-                @max_width = '100%'
-              end
-              html = render_to_string "main/_image", layout: false
-            else
-              html = "failed to render media"
+    new_string = ""
+    regexp = /
+      \A
+      MEDIA:(?<filetype>img|img_full|video|yt|audio):
+      (?<id>[a-zA-Z0-9|./_]+):"(?<caption>.*)":?(?<width>[0-9px%]*)
+      /x
+    @html.each_line do |str|
+      m = regexp.match(str)
+      if m
+        # file tag found
+        # replace with corresponding html :)
+        # m[0] whole tag
+        @caption = m[:caption]
+        new_html = ""
+        case m[:filetype]
+        when 'img'
+          @img_id += @img_id_inc
+          @imgset = Array.new
+          if m[:id].count('|') == @caption.count('|')
+            m[:id].split('|').each do |url|
+              @imgset.push Image.find("url", m[:id]).split('|'))
             end
-          when 'img_full'
-            @image = ImageFile.find(m[:id])
-            html = render_to_string "main/_image_full", layout: false
-          when 'video'
-            @video = VideoFile.find(m[:id])
-            html = render_to_string "main/_video", layout: false
-          when 'audio'
-            @audio = AudioFile.find(m[:id])
-            html = render_to_string "main/_audio", layout: false
-          when 'yt'
-            @yt_id = m[:id]
-            html = render_to_string "main/_youtube", layout: false
+            @img_id_inc = @imgset.size
+            #@images = ImageFile.find(m[:id].split('|'))
+            unless m[:width].empty?
+              @max_width = m[:width]
+            else
+              @max_width = '100%'
+            end
+            a = ZERB.new("_layouts/_image.html.erb")
+            a.set_data(:img_id, @img_id)
+            a.set_data(:imgset, @imgset)
+            a.set_data(:max_width, @max_width)
+            a.set_data(:caption, @caption)
+            a.prepare
+            a.render
+            html = a.output
           else
             html = "failed to render media"
           end
-          html.each_line do |substr|
-            if substr.lstrip
-              new_html << substr.lstrip
-            else
-              new_html << substr
-            end
-          end
-          str.sub! m[0], new_html.chomp!
+        when 'img_full'
+          @image = Image.find(url, m[:id])
+          a = ZERB.new("_layouts/_image_full.html.rb")
+          a.set_data(:image, @image)
+          a.set_data(:caption, @caption)
+          a.prepare
+          a.render
+          html = a.output
+        when 'video'
+          @video = VideoFile.find(m[:id])
+          a = ZERB.new("_layouts/_video.html.rb")
+          a.set_data(:video, @video)
+          a.set_data(:caption, @caption)
+          a.prepare
+          a.render
+          html = a.output
+        when 'audio'
+          @audio = AudioFile.find(m[:id])
+          a = ZERB.new("_layouts/_audio.html.rb")
+          a.set_data(:audio, @audio)
+          a.set_data(:caption, @caption)
+          a.prepare
+          a.render
+          html = a.output
+        when 'yt'
+          @yt_id = m[:id]
+          a = ZERB.new("_layouts/_youtube.html.rb")
+          a.set_data(:yt_id, @yt_id)
+          a.set_data(:caption, @caption)
+          a.prepare
+          a.render
+          html = a.output
+        else
+          html = "failed to render media"
         end
-        new_string << str
+        html.each_line do |substr|
+          if substr.lstrip
+            new_html << substr.lstrip
+          else
+            new_html << substr
+          end
+        end
+        str.sub! m[0], new_html.chomp!
       end
-
-      markdown = Redcarpet::Markdown.new(RougeHTML,
-                                         autolink: true)
-      return markdown.render(new_string)
-    else
-      return nil
+      new_string << str
     end
+
+    markdown = Redcarpet::Markdown.new(RougeHTML,
+                                       autolink: true)
+    @html = markdown.render(new_string)
   end
 
   private
