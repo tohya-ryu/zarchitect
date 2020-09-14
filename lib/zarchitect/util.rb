717bf93b6963612d688a6657061e79c42193c209
ryu <ryu@tohya.net>
2020-09-14 15:44:43 +0200

    begin improving Util.mkdir

--- lib/zarchitect/util.rb
+++ lib/zarchitect/util.rb
@@ -1,6 +1,26 @@
 module Util
 
   def self.mkdir(path)
+    a = path.split("/")
+    i = 0
+    a.each do |s|
+      if i > 0
+      else
+        Util.mkdir2(s)
+      end
+      i += 1
+    end
+
+  end
+  
+  # path to data files located in installation directory
+  def self.path_to_data
+    File.join(__dir__, "../../data")
+  end
+
+  # PRIVATE
+
+  def self.mkdir2(path)
     npath = File.join(Dir.getwd, path)
     GPI.print "Checking for directory #{npath}", GPI::CLU.check_option('v')
     unless Dir.exist?(npath)
@@ -9,11 +29,6 @@ module Util
       Dir.mkdir(npath)
       GPI.print "Created directory #{npath}", GPI::CLU.check_option('v')
     end
-  end
-  
-  # path to data files located in installation directory
-  def self.path_to_data
-    File.join(__dir__, "../../data")
   end
 
 end
