2ba3efa0fe01ec4dc5d86e8d8738c7609d83277c
ryu <ryu@tohya.net>
2020-10-06 10:23:06 +0200

    add lines to syntax highlighting

--- lib/zarchitect/rouge_html.rb
+++ lib/zarchitect/rouge_html.rb
@@ -8,8 +8,28 @@ class RougeHTML < Redcarpet::Render::HTML
     line = code.lines.first
     ar = line.split ':'
     code = code.lines.to_a[1..-1].join
-    str = "<pre><code class='highlight'>"
-    str << Rouge.highlight(code, ar[1].chomp, 'html')
-    str << "</code></pre>"
+
+    # add highlighting
+    str = Rouge.highlight(code, ar[1].chomp, 'html')
+
+    # add line numbers
+    code2 = ""
+    i = 1
+    str.each_line do |l|
+      j = "#{i}"
+      if j.length == 1
+        j.prepend("00")
+      elsif j.length == 2
+        j.prepend("0")
+      end
+      code2 << "<span style='user-select:none;-moz-user-select:none;'>" +
+               "#{j}  </span>#{l}" 
+      i += 1
+    end
+
+    # finalize
+    code2.prepend("<pre><code class='highlight'>")
+    code2 << "</code></pre>"
+
   end
 end
