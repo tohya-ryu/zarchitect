d7b81a3481a23253a984392d81d8ae1a86547aa9
ryu <ryu@tohya.net>
2020-10-14 12:31:13 +0200

    move helper functions to Zarchitect

--- lib/zarchitect.rb
+++ lib/zarchitect.rb
@@ -54,6 +54,30 @@ class Zarchitect
     @@rss
   end
 
+  private
+
+  def self.rebuild
+    # delete all contents of _html
+    Dir[ File.join("_html", "**", "*") ].reverse.reject do |fullpath|
+      if File.directory?(fullpath)
+        GPI.print "deleting dir #{fullpath}"
+        Dir.delete(fullpath)
+        GPI.print "deleted dir #{fullpath}"
+      else
+        GPI.print "deleting file #{fullpath}"
+        File.delete(fullpath)
+        GPI.print "deleted file #{fullpath}"
+      end
+    end
+  end
+
+  def self.setup_html_tree
+    Util.mkdir("_html")
+    Util.mkdir("_html/assets")
+    Util.mkdir("_html/files")
+    Util.mkdir("_build/debug") if GPI::CLU.check_option('d')
+  end
+
   def load_conf
     @zr_config = Config.new("_config/_zarchitect.yaml")
     @zr_config.validate_zrconf
