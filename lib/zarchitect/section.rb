014d3a2ccfe694e0a034c62451cec774366b528e
ryu <ryu@tohya.net>
2020-10-16 12:29:45 +0200

    initialize Section

--- lib/zarchitect/section.rb
+++ lib/zarchitect/section.rb
@@ -1,8 +1,20 @@
 class Section < Zarchitect
 
   def initialize(conf)
+    GPI.print "Initializing Section #{conf.key}.", GPI::CLU.check_option('v')
     @conf = conf
+    @name = @conf.name.clone
+    @pages = Array.new
+    @categories = Array.new
+    @index = Array.new
+    if @conf.index
+      @url = "/index.html"
+    else
+      @url = "/#{@conf.key}/index.html"
+    end
     create_dir
+    fetch_pages
+    set_categories
   end
 
   private
