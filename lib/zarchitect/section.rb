6439eccd05fab9e4e94a390f5fb172931566c6a7
ryu <ryu@tohya.net>
2020-10-16 11:22:12 +0200

    separate method to create sec dir

--- lib/zarchitect/section.rb
+++ lib/zarchitect/section.rb
@@ -2,8 +2,14 @@ class Section < Zarchitect
 
   def initialize(conf)
     @conf = conf
+    create_dir
+  end
+
+  private
+
+  def create_dir
     unless @conf.index
-      Util.mkdir(File.join(HTMLDIR, @conf.name))
+      Util.mkdir(File.join(HTMLDIR, @conf.key))
     end
   end
 
