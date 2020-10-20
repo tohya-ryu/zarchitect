0919fe41c8fe9fe88eba8a9251533930212745ef
ryu <ryu@tohya.net>
2020-10-20 11:15:09 +0200

    set Post skeleton

--- lib/zarchitect/post.rb
+++ lib/zarchitect/post.rb
@@ -5,7 +5,12 @@ class Post < Zarchitect
     @source_path = path
     @conf = Config.new(path)
     @conf.validate_post
-    
+    # read content
+    # set title
+    # set url
+    # construct path for html // maybe only necessray when writing html?
+    # set date
+    rss.try_item(self)
   end
 
 end
