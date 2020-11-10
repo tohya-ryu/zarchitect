cfb3a75714e300d8b99186128bb6bc9549fdc794
ryu <ryu@tohya.net>
2020-11-10 14:39:55 +0100

    fix

--- lib/zarchitect/section.rb
+++ lib/zarchitect/section.rb
@@ -96,7 +96,7 @@ class Section < Zarchitect
       @posts = Array.new
       if @conf.has_option?("directory")
         Dir.filesr(@conf.directory).each do |f|
-          next unless File.extname(f)
+          next unless File.extname(f) == ".md"
           @posts.push Post.new(f, self)
         end
       elsif @conf.has_option?("file")
